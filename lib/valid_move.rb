f92509d397cd23119851560c1ca68565fee26fee
Austin Wolfe <wolfe_austin@yahoo.com>
2018-02-20 21:38:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,5 +3,5 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  board[index] != "" && board[index] != " "&& board[index] != nil
+  board[index] == "" && board[index] != " "&& board[index] != nil
 end 
